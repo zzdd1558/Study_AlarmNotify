40b25539ff957c34e90206278dc188b28d903a27
yunjin choi <zzdd1558@naver.com>
2018-08-02 20:07:31 +0900

    git group 추가

--- app/src/main/java/com/example/yunjin_choi/notifydemoactivity/MainActivity.java
+++ app/src/main/java/com/example/yunjin_choi/notifydemoactivity/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     private Button sendNotify;
+    private static String GROUP_KEY_NOTIFY = "Group Key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 sendNotification();
                 break;
+
         }
     }
 
@@ -46,11 +48,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+        NotificationCompat.Builder builderSummary = new NotificationCompat.Builder(this)
                 .setSmallIcon(android.R.drawable.ic_dialog_info)
                 .setContentTitle("Notify Test")
                 .setContentText("안녕하세요 Notify 테스트입니다.")
+                .setGroup(GROUP_KEY_NOTIFY)
+                .setGroupSummary(true)
+                //사운드 추가시 사용 기본 default 사운드 나옴.
+                .setSound(defaultSoundUri);
 
+        NotificationCompat.Builder builder1 = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("new First Notify Test")
+                .setContentText("안녕하세요 Notify1 입니다.")
+                .setGroup(GROUP_KEY_NOTIFY)
+                //사운드 추가시 사용 기본 default 사운드 나옴.
+                .setSound(defaultSoundUri);
+
+        NotificationCompat.Builder builder2 = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("new Second Notify Test")
+                .setContentText("안녕하세요 Notify2 입니다.")
+                .setGroup(GROUP_KEY_NOTIFY)
+                //사운드 추가시 사용 기본 default 사운드 나옴.
+                .setSound(defaultSoundUri);
+
+        NotificationCompat.Builder builder3 = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("new Third Notify Test")
+                .setContentText("안녕하세요 Notify3 입니다.")
+                .setGroup(GROUP_KEY_NOTIFY)
                 //사운드 추가시 사용 기본 default 사운드 나옴.
                 .setSound(defaultSoundUri);
 
@@ -65,18 +92,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         PendingIntent pendingIntent = PendingIntent.getActivity(this , 0 , resultIntent , PendingIntent.FLAG_UPDATE_CURRENT);
 
         // notificationCompat.Builder에 setContentIntent메서드의 매개변수로 PendingIntent를 넣어준다.
-        builder.setContentIntent(pendingIntent);
+        builderSummary.setContentIntent(pendingIntent);
 
 
         // 알림에 액션 추가하기
         NotificationCompat.Action action = new NotificationCompat.Action.Builder(android.R.drawable.sym_action_chat , "OPEN" , pendingIntent).build();
-        builder.addAction(action);
+        builderSummary.addAction(action);
 
-        int notificationId = 101;
+
+        //숫자가 같다면 마지막에 등록된 Notify로 대체된다
+        // 덮어씌운건지 마지막걸로 Notify를 해주는지 확인 필요.
+        int notificationSummary = 101;
+        int notificationId1 = 102;
+        int notificationId2 = 103;
+        int notificationId3 = 104;
 
         NotificationManager notifyMgr =
                 (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
-        notifyMgr.notify(notificationId , builder.build());
+        notifyMgr.notify(notificationSummary , builderSummary.build());
+        notifyMgr.notify(notificationId1 , builder1.build());
+        notifyMgr.notify(notificationId2 , builder2.build());
+        notifyMgr.notify(notificationId3 , builder3.build());
+
     }
 }
